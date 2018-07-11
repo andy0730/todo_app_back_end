c1b3cea8c478d9faf81afcb2feff5c72fffb5e48
Andrew Park <andrewpark9909@gmail.com>
2018-07-11 15:28:46 +0000

    using a tags for index.html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,38 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// $(document.ready(function(){
+//     $("ul").on("click",input[type="checkbox"],function(){
+//         $(this).closest("li").togleClass("completed-todo")}}
+        
+
+    console.log ("hello world");
+   
+    var pomodoros= $("#pomodoros").val(); 
+    
+    var name_of_todo= $("#name_of_todo").val();
+
+    // $(document).ready(console.log("Hello World"));
+    
+    // $(document).ready($("#add_to_do_button").click(function(){console.log("this works");}))
+    
+    // $(document).ready(function(){console.log("this works");
+    // $("add_to_do_button").click(function(){console.log("this works")})});
+    
+    
+    $(document).ready(function(){
+        console.log("hello")
+        $("#add_to_do_button").click(function(){
+            console.log("button works")
+            var name_of_todo= $("#name_of_todo").val()
+            var pomodoros= $("#number_of_pomodoros").val()
+            console.log(name_of_todo) 
+            console.log(pomodoros)
+            $("ul").append("<li><input type='checkbox'/>"+name_of_todo+"<span class='pomodoros'>"+pomodoros+"</span></li>");
+        })
+        $("input[type='checkbox']").click(function(){
+            $(this).closest('li').toggleClass('completedtasks')
+            console.log("hi")
+        })
+    })
