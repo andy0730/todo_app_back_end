cdc9f57ef84132d3fb0ad38b175e219eceb8c8f8
Andrew Park <andrewpark9909@gmail.com>
2018-07-11 17:49:03 +0000

    Install postgres and update database.yml

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,24 +15,15 @@
 //= require turbolinks
 //= require_tree .
 
+
 // $(document.ready(function(){
 //     $("ul").on("click",input[type="checkbox"],function(){
 //         $(this).closest("li").togleClass("completed-todo")}}
         
-
-    console.log ("hello world");
    
     var pomodoros= $("#pomodoros").val(); 
     
     var name_of_todo= $("#name_of_todo").val();
-
-    // $(document).ready(console.log("Hello World"));
-    
-    // $(document).ready($("#add_to_do_button").click(function(){console.log("this works");}))
-    
-    // $(document).ready(function(){console.log("this works");
-    // $("add_to_do_button").click(function(){console.log("this works")})});
-    
     
     $(document).ready(function(){
         console.log("hello")
