092b87b165a8c649990b869e71f80e9d5a89a838
Andrew Park <andrewpark9909@gmail.com>
2018-07-11 15:13:23 +0000

    make conditional logic work

--- app/controllers/concerns/todo_controller.rb
+++ app/controllers/concerns/todo_controller.rb
@@ -2,10 +2,27 @@ class TodoController < ApplicationController
     def index
     end 
     def show
-        @todo_name="go to korea"
-        @todo_pomodoro= 13
+        @todo_list_section="france won the world cup"
+        @pomodoros= 3
+        todo_id=params[:id]
+        if todo_id =='1'
+            @todo_list_section="practice violin"
+            @pomodoros=2
+        elsif todo_id =='2'
+            @todo_list_section="go to korea"
+            @pomodoros=2
+        elsif todo_id =='3'
+            @todo_list_section="read"
+            @pomodoros=2
+        elsif todo_id =='4'
+            @todo_list_section="finish homework"
+            @pomodoros=2     
+        elsif todo_id =='5'
+            @todo_list_section="buy sister her b-day present"
+            @pomodoros=2    
+        end
+            #when user goes to todo/show/1 @todo_list_section will be practice violin and @pomodoros will be 2
+            #when user goes to todo/show/2 @todo_list_section will be finish homework and @pomodoros will be 2
+            #when user goes to todo/show/3 @todo_list_section will be read and @pomodoros will be 2
     end
 end 
-   
-    
-    
