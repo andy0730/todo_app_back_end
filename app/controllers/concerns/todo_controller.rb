181f3850cc5a2bc28033319b65e6c13c298b1120
Andrew Park <andrewpark9909@gmail.com>
2018-07-12 13:10:13 +0000

    change rails to 2.8

--- app/controllers/concerns/todo_controller.rb
+++ app/controllers/concerns/todo_controller.rb
@@ -16,8 +16,8 @@ class TodoController < ApplicationController
     end
     def create
         t=Todo.new 
-        t.description= params[:description]
-        t.pomodoro_estimate= params[:pomodoro_estimate]
+        t.description= params['description']
+        t.pomodoro_estimate= params['pomodoro_estimate']
         t.save
         redirect_to "/todo/index"
     end
