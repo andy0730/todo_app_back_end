72648861d3ff4a79c5a32cd5e528d341d3f54977
Andrew Park <andrewpark9909@gmail.com>
2018-07-12 12:22:02 +0000

    finishing touch

--- app/controllers/concerns/todo_controller.rb
+++ app/controllers/concerns/todo_controller.rb
@@ -4,25 +4,21 @@ class TodoController < ApplicationController
     def show
         @todo_list_section="france won the world cup"
         @pomodoros= 3
-        todo_id=params[:id]
-        if todo_id =='1'
-            @todo_list_section="practice violin"
-            @pomodoros=2
-        elsif todo_id =='2'
-            @todo_list_section="go to korea"
-            @pomodoros=2
-        elsif todo_id =='3'
-            @todo_list_section="read"
-            @pomodoros=2
-        elsif todo_id =='4'
-            @todo_list_section="finish homework"
-            @pomodoros=2     
-        elsif todo_id =='5'
-            @todo_list_section="buy sister her b-day present"
-            @pomodoros=2    
-        end
+        @todo= Todo.find_by_id(params[:id]) 
             #when user goes to todo/show/1 @todo_list_section will be practice violin and @pomodoros will be 2
             #when user goes to todo/show/2 @todo_list_section will be finish homework and @pomodoros will be 2
             #when user goes to todo/show/3 @todo_list_section will be read and @pomodoros will be 2
     end
-end 
+    def edit
+        
+    end
+    def new
+    end
+    def create
+        t=Todo.new 
+        t.description= params[:description]
+        t.pomodoro_estimate= params[:pomodoro_estimate]
+        t.save
+        redirect_to "/todo/index"
+    end
+end
